1db22e0bebe9f8f9fe78968b3d062a770d55d250
Eric Huang <yiidahuang@outlook.com>
2023-03-18 22:47:43 +0800

    feat: add mongoose 使用mongoose建立連線，並利用dotenv來管理連線字串

---
+++ models/todo.js
@@ -0,0 +1,15 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const todoSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    done: {
+        type: Boolean
+    }
+})
+
+//使用mongoose.model方法，會把定義的Schema編譯程一個可供操作的model物件，名為Todo
+module.exports = mongoose.model('Todo', todoSchema);
