7d65217e34ab9a58073727c9e3a9eee2191d9069
Eric Huang <86519473+erikku54@users.noreply.github.com>
2023-07-15 19:14:26 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,14 @@ const router = express.Router();
 
 const home = require('./modules/home');
 const todos = require('./modules/todos');
+const users = require('./modules/users');
 
 // 網址符合'/todos'的request導向todo路由
-router.use('/todos',todos)
+router.use('/todos',todos);
+// 網址符合'/users'的request導向users路由
+router.use('/users', users);
 // 網址符合'/'的request導向home路由
-router.use('/', home)
+router.use('/', home);
 
 
 module.exports = router;
