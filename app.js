7af61be3961ac955a0caa8afcfdc31c5412ca3f7
Eric Huang <86519473+erikku54@users.noreply.github.com>
2023-07-16 23:24:38 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const exphbs = require('express-handlebars').engine;
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
+// 載入body-parser
+app.use(express.urlencoded({ extended: true }));
 
-// 設定使用session儲存機制，其中options物件的secret是驗證字串（最必要設定）
+
+// 載入設定使用session儲存機制，其中options物件的secret是驗證字串（最必要設定）
 // resave是每次使用者互動後強制更新session，saveUninitialized是強制將未初始化的session存回（這兩項沒設會跳警告）
 const session = require('express-session');
 app.use(session({
@@ -18,14 +21,15 @@ app.use(session({
     saveUninitialized: true,
 }));
 
-
-app.use(express.urlencoded({ extended: true }));
-
-
-const methodOverride = require('method-override');  //載入
+//載入method-override
+const methodOverride = require('method-override');  
 app.use(methodOverride('_method'));  //設定每一筆請求都會透過method-override進行前處理
 
+// 載入passport使用者驗證機制
+const usePassport = require('./config/passport');
+usePassport(app);
 
+// 載入路由
 const routes = require('./routes');
 app.use(routes);
 
---
+++ config/passport.js
@@ -0,0 +1,40 @@
+
+const User = require('../models/user');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;   // 此處.Strategy可以省略不寫，因為此package預設輸出就是Strategy
+
+module.exports = app => {
+    // 初始化Passport模組
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    // 設定本地登入策略
+    passport.use(new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
+        User.findOne({ email })
+            .then(user => {
+                if (!user) {
+                    // 如果使用者不存在
+                    return done(null, false, { message: 'This email is not registered!'});
+                }
+                if (user.password !== password) {
+                    // 如果密碼不正確
+                    return done(null, false, { message: 'Email or Password incorrect.'});
+                }
+                // 非上兩者為成功登入
+                return done(null, user);
+            }).catch(err => done(err, false))
+    }))
+
+    // 設定序列化及反序列化
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    })
+    passport.deserializeUser((id, done) => {
+        User.findById(id)
+            .lean()     // 後續物件可能傳進前端樣板，所以先把資料庫物件換成JS物件
+            .then(user => done(null, user))
+            .catch(err => done(err, null));
+    })
+}
+
