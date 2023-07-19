5d7751815432d01b27c8d075954bb98bdae397a4
Eric Huang <86519473+erikku54@users.noreply.github.com>
2023-07-19 22:49:04 +0800

    fix: correct facebook callback url

--- config/passport.js
+++ config/passport.js
@@ -35,7 +35,7 @@ module.exports = app => {
     passport.use(new FacebookStrategy({ 
         clientID: process.env.FACEBOOK_ID,
         clientSecret: process.env.FACEBOOK_SECRET,
-        callbackURL: process.env.CALLBACK,
+        callbackURL: process.env.FACEBOOK_CALLBACK,
         profileFields: ['email', 'displayName']
     }, (accessToken, refreshToken, profile, done) => {
         // console.log(profile);
